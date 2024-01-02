6b00d9921520df6b0d9661a3b650d746d6f611f1
Kreida Anton <kreida.anton@yandex.ru>
2024-01-02 20:03:50 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx}",
+    "./src/hooks/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import * as path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react(), svgr({ svgrOptions: {
+      exportType:"default"
+      }
+    }), checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint \"./**/*.{ts,tsx}\"",
+      },
+    })],
+    preview: {
+      port: +(process.env.VITE_PORT ?? '3000'),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+        "@shared": path.resolve(__dirname, "./src/shared/"),
+        "@interfaces": path.resolve(__dirname, "./src/interfaces/"),
+        "@components": path.resolve(__dirname, "./src/components/"),
+        "@layouts": path.resolve(__dirname, "./src/layouts/"),
+        "@pages": path.resolve(__dirname, "./src/pages/"),
+        "@assets": path.resolve(__dirname, "./src/assets/"),
+        "@context": path.resolve(__dirname, "./src/context/"),
+        "@styles": path.resolve(__dirname, "./src/styles/"),
+        "@hooks": path.resolve(__dirname, "./src/hooks/"),
+        "@api": path.resolve(__dirname, "./src/api/"),
+        "@hocs": path.resolve(__dirname, "./src/hocs/"),
+        "@api-hooks": path.resolve(__dirname, "./src/api-hooks/")
+      },
+    },
+    server: {
+      port: +(process.env.VITE_PORT ?? '3000'),
+      host: true,
+    },
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    },
+  });
+}
+
